3d22100ce0280f38a15efd07ccfbe64d690e4cc9
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-30 17:22:40 +0900

    +

--- 2_Run/1st_Part.py
+++ 2_Run/1st_Part.py
@@ -76,3 +76,11 @@ for x in range(my_time, 0, -1):
     time.sleep(1)
 print('Sleep')
 '''
+
+'''
+def display_invoice(username, amount, due_date):
+    print(f'Hello {username}')
+    print(f'Your bill of ${amount:.2f} is due: {due_date}')
+
+display_invoice('BroCode', 42.50, "01/02")'''
+
