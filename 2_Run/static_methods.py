c9a41093ec4d4e6d38e976b61ff46a250fcc0dde
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-10-03 17:24:10 +0900

    +

--- 2_Run/static_methods.py
+++ 2_Run/static_methods.py
@@ -71,3 +71,45 @@ print(Employee.is_high_salary(4000))
 print(Employee.is_high_salary(7000))
 
 print("Total employees:", Employee.employee_count)
+
+
+class Book:
+
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    def __eq__(self, other):
+        return self.title == other.title & self.author == other.author
+    
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+    
+    def __gt__(self, other):
+        return self.num_pages > other.num_pages
+    
+    def __add__(self, other):
+        return f"{self.num_pages + other.num_pages} pages"
+    
+    def __contains__(self, keyword):
+        return keyword in self.title | keyword in self.author
+    
+    def __getitem__(self, key):
+        if key == 'title':
+            return self.title
+        elif key == 'author':
+            return self.author
+        elif key == 'num_pages':
+            return self.num_pages
+        else:
+            return f"Key '{key}' was not found"
+    
+    
+
+book1 = Book('The Hobbit', 'J.R.R. Tolkien', 310)
+book2 = Book('Haayr poter', 'Someone2', 223)
+book3 = Book('Book2', 'Someone3', 172)
