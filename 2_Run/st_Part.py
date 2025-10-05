8619becb898303aabeb70f112e3015bc8847de31
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-10-05 17:38:43 +0900

    python finished again:)

--- 2_Run/st_Part.py
+++ 2_Run/st_Part.py
@@ -412,3 +412,36 @@ def main():
 if __name__ == '__main__':
     main()
 '''
+
+'''
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(500,300,400,400)
+
+        label = QLabel(self)
+        label.setGeometry(0,0,250,250)
+
+        pixmap = QPixmap('007.jpg')
+        label.setPixmap(pixmap)
+
+        label.setScaledContents(True)
+
+        label.setGeometry((self.width() - label.width()) // 2,
+                          (self.height() - label.height()) // 2,
+                          label.width(),
+                          label.height())
+    
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
+    '''
