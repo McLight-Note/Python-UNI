056283741aa6ed053c1b58ae43acec957b2d6896
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-10-05 16:25:07 +0900

    ++

--- 2_Run/st_Part.py
+++ 2_Run/st_Part.py
@@ -351,9 +351,9 @@ print(now)
 import threading
 import time
 
-def walk_dog():
+def walk_dog(first, last):
     time.sleep(8)
-    print('You finish walking the dog')
+    print(f'You finish walking the {first} {last}')
 
 def take_out_trash():
     time.sleep(2)
@@ -363,7 +363,7 @@ def get_mail():
     time.sleep(4)
     print('You get the mail')
 
-chore1 = threading.Thread(target=walk_dog)
+chore1 = threading.Thread(target=walk_dog, args=('Scooby', "Doo"))
 chore1.start()
 
 chore2 = threading.Thread(target=take_out_trash)
@@ -372,4 +372,7 @@ chore2.start()
 chore3 = threading.Thread(target=get_mail)
 chore3.start()
 
+chore1.join()
+chore2.join()
+chore3.join()
 print('All chores are complete')
