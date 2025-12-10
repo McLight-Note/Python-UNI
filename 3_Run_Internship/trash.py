a00f716e12f459312df75ad5a7b51995d0cc435c
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-12-10 21:12:47 +0900

    topKFrequentNumbers

--- 3_Run_Internship/trash.py
+++ 3_Run_Internship/trash.py
@@ -1,1 +1,13 @@
-print(1234*10 + 12%10,1234%10)
+class Solution:
+    def topKFrequent(self, nums, k):
+        freq = {}
+        
+        for num in nums:
+            if num not in freq:
+                freq[num] = 0
+            freq[num] += 1
+
+        sorted_nums = sorted(freq, key=lambda x: freq[x], reverse=True)
+        return sorted_nums[:k]
+
+print(Solution().topKFrequent([1,2,2,3,3,3], 2))
