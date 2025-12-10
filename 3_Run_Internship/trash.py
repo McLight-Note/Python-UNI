b9e4479b17aa8b913905f634a6a3d538fefaf86d
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-12-10 23:48:12 +0900

    productExceptSelf

--- 3_Run_Internship/trash.py
+++ 3_Run_Internship/trash.py
@@ -1,13 +1,32 @@
-class Solution:
-    def topKFrequent(self, nums, k):
-        freq = {}
+# class Solution:
+#     def topKFrequent(self, nums, k):
+#         freq = {}
         
-        for num in nums:
-            if num not in freq:
-                freq[num] = 0
-            freq[num] += 1
+#         for num in nums:
+#             if num not in freq:
+#                 freq[num] = 0
+#             freq[num] += 1
 
-        sorted_nums = sorted(freq, key=lambda x: freq[x], reverse=True)
-        return sorted_nums[:k]
 
-print(Solution().topKFrequent([1,2,2,3,3,3], 2))
+#         sorted_nums = sorted(freq, key=lambda x: freq[x], reverse=True)
+#         return sorted_nums[:k]
+
+# print(Solution().topKFrequent([1,2,2,3,3,3], 2))
+
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        res = [1] * n
+
+        left = 1
+        for i in range(n):
+            res[i] = left
+            left *= nums[i]
+        
+        right = 1
+        for i in range(n - 1, -1, -1):
+            res[i] = right
+            right *= nums[i]
+        return res
+
+print(Solution().productExceptSelf([1,2,4,6]))
