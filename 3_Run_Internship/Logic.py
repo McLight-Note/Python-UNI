75aff914fe7f9bea91d36664835848d20ef0e8cf
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-12-11 20:21:32 +0900

    +

--- 3_Run_Internship/Logic.py
+++ 3_Run_Internship/Logic.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(1, len(prices)):
             if prices[i] > prices[i-1]:
                 profit += prices[i] - prices[i-1]
-        return profit 
+        return profit
 
 print(Solution().maxProfit([7,1,5,3,6,4]))
 print(Solution().maxProfit([1,2,3,4,5]))
